a53d8fb99e8b8bf50dda905da3cc932d01b500b9
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-02-21 04:45:33 -0800

    solved task 9 again

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,9 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			if (product / 10 == 0 && j == 0)
-				_putchar('\0');
-			else if (product / 10 == 0)
+			if (product / 10 == 0 && j != 0)
 				_putchar(' ');
-			else
+			else if (product / 10 > 0)
 				_putchar(product / 10 + '0');
 			_putchar(product % 10 + '0');
 			if (j != 9)
