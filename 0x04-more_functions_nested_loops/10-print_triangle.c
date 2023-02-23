e59f76cb97f3e79710b811c9d82257d8e6de87e3
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-02-23 12:37:19 -0800

    solved task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints '#' n triangle times
+ * @size: int size parameter
+ * Description: it takes advantage of the _putchar function
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+				_putchar(' ');
+			for (j = 0; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+}
