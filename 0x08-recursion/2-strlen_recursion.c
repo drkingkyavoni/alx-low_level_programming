1b25b52be55524248449c6c130922841f0df9b38
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-03-08 00:33:14 -0800

    solved tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * Description: uses pointers
+ * @s: a char pointer parameter
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	return(strlen(s));
+}
