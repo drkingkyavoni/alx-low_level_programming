8d0826bb9f0ce2da46d47b0ee2f122163f818e8d
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-03-13 09:08:02 -0700

    solved again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			if ((atoi(argv[i]) == 0) &&
-				((*argv[i] < 45) ||
+				((*argv[i] < 48) ||
 				 (*argv[i] > 57)))
 			{
 				printf("Error\n");
