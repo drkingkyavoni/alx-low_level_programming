9d9ab849cd350af443c1a213f76fd4469715627d
Kyuka <drkyavoni@live.com>
2023-06-07 20:07:23 -0700

    fix betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+
+/**
+ * _strlen - counts length of string
+ * @s: char pointer
+ * Return: int
+*/
 int _strlen(char *s)
 {
 	char *_s = s;
@@ -7,6 +13,13 @@ int _strlen(char *s)
 	return (int)(s - _s);
 }
 
+/**
+ * test_palindrome - checks if string is palindrome
+ * @s: const char pointer
+ * @start: int
+ * @last: int
+ * Return: int
+*/
 int test_palindrome(const char *s, int start, int last)
 {
 	if (start >= last) return (1);
@@ -14,6 +27,10 @@ int test_palindrome(const char *s, int start, int last)
 	return (test_palindrome(s, start + 1, last - 1));
 }
 
+/**
+ * is_palindrome - checks if string s is palindrome
+ * Return: int
+*/
 int is_palindrome(char *s)
 {
 	if (test_palindrome(s, 0, _strlen(s) - 1)) return 1;
