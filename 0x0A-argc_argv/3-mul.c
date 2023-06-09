fa80f506eeeec733a281241c8e9157ce4ae479f8
Kyuka <drkyavoni@live.com>
2023-06-09 15:38:41 -0700

    tasks completed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char pointer
+ * Return: int
+*/
+int main(int argc, char **argv)
+{
+	int sum = 1;
+
+	*argv++;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	while (*argv)
+	{
+		sum *= atoi(*argv);
+		*argv++;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
+
