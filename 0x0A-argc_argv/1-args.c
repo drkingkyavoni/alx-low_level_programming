b81fb98b3b77f2f88551528e06f4dba6c108cc63
Kyuka <drkyavoni@live.com>
2023-06-10 01:48:45 -0700

    fixed task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define NLF(i) (void)(i)
+
 
 /**
  * main - entry point
@@ -8,7 +10,7 @@
 */
 int main(int argc, char **argv)
 {
-	(void) argv;
+	NLF(argv);
 	printf("%d\n", argc);
 	return (0);
 }
