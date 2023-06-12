80d71cb9d6733a9432cf2bdefcce1952a15af755
Kyuka <drkyavoni@live.com>
2023-06-12 02:43:14 -0700

    modified task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,20 +13,20 @@ int main(int argc, char **argv)
 	char *lastptr;
 
 	coins = 0;
-	if (argc != 2)
+	if (argc != 2) /* check that only two arguments allowed */
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	bal = strtol(argv[1], &lastptr, 10);
+	bal = strtol(argv[1], &lastptr, 10); /* check if digit */
 	if (lastptr == *argv || *lastptr != '\0' || bal < 0)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	while (bal)
+	while (bal) /* count coins for each debit */
 	{
 		bal -=  (bal >= 25) ? 25 :
 		(bal >= 10) ? 10 :
