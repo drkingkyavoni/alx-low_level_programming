d12cdac2a7a8e02b24585eead728b0b076709775
Kyuka <drkyavoni@live.com>
2023-06-13 23:41:30 -0700

    solved

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,34 +3,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-int size, i;
-char *temp, *s;
 
 /**
-* argstostr - concatenates strings 
+* argstostr - concatenates strings
 * @ac: int
 * @av: char pointer
 * Return: char pointer
 */
 char *argstostr(int ac, char **av)
 {
-    if (ac == 0 || av == NULL)
-        return (NULL);
-    
-    for (i = 0; i < ac; i++)
-        size += (strlen(av[i]) + 1);
+	int size, i;
+	char *temp, *s;
 
-    temp = (char *)malloc(sizeof(char) * (size + 1));
-    if (!temp)
-        return (NULL);
+	size = 0;
 
-    s = temp;
-    strcpy(temp, "");
-    for (i = 0; i < ac; i++)
-    {
-        strcat(temp, av[i]);
-        strcat(temp, "\n");
-    }
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    return (s);
+	for (i = 0; i < ac; i++)
+		size += (strlen(av[i]) + 1);
+
+	temp = (char *)malloc(sizeof(char) * (size + 1));
+
+	if (!temp)
+		return (NULL);
+
+	s = temp;
+	strcpy(temp, "");
+	for (i = 0; i < ac; i++)
+	{
+		strcat(temp, av[i]);
+		strcat(temp, "\n");
+	}
+
+	return (s);
 }
