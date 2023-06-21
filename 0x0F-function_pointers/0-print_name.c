25cca0a4268e2519001e578e830348e7f075319f
Kyuka <drkyavoni@live.com>
2023-06-21 11:00:52 -0700

    solved

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 
-
 /**
  * print_name - function that prints a name
  * @name: char pointer
@@ -9,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if (name != NULL && f != NULL && *name != '\0')
 		f(name);
 }
 
