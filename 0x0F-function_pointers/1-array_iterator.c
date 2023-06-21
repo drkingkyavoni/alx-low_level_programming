5ba704ede269c5184f5d8fc2b5126e8b32c38eab
Kyuka <drkyavoni@live.com>
2023-06-21 11:04:27 -0700

    solved

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (array != NULL && action != NULL && size > 0)
+	{
 		size_t i;
 
 		for (i = 0; i < size; i++)
 			action(array[i]);
+	}
 }
 
