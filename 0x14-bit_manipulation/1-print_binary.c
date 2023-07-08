9c976c32c71c4a1dff6b41de2caec66eacf4b9ed
Kyuka <drkyavoni@live.com>
2023-07-08 06:10:05 -0700

    solved

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * print_binary - prints the binary of a number
- * @n: unsigned long int
- * Return: void
- */
-void print_binary(unsigned long int n)
-{
-	if (!n)
-		_putchar('0');
-
-	f(n);
-}
 
 /**
  * f - prints the binary in reverse
@@ -25,5 +13,21 @@ void f(unsigned long int n)
 		return;
 	f(n >> 1);
 	_putchar((n & 1) ? '1' : '0');
+}
+
+/**
+ * print_binary - prints the binary of a number
+ * @n: unsigned long int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+
+	f(n);
 }
 
