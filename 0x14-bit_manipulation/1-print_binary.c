0492b07dd96b56e86a0d21eb384610e9118b6fa8
Kyuka <drkyavoni@live.com>
2023-07-08 03:58:17 -0700

    modified

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,22 +10,11 @@ void print_binary(unsigned long int n)
 {
 	if (!n)
 		_putchar('0');
-	else
-		print_n_binary(n);
-}
 
-/**
- * print_n_binary - prints the binary of a number
- * @n: unsigned long int
- * Return: void
- */
-void print_n_binary(unsigned long int n)
-{
-	if (n)
-		print_n_binary(n >> 1);
-	else
-		_putchar('\0');
-
-	_putchar((n & 1) ? '1' : '0');
+	while (n > 0)
+	{
+		_putchar((n & 1) ? '1' : '0');
+		n >>= 1;
+	}
 }
 
