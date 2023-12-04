b70775176d3010513cff1928969c49d86cad829f
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-12-04 17:35:44 +1300

    v0.1.2

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_print - function that prints a hash table
+ *
+ * @ht: const hash_table_t *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, printed;
+	hash_node_t *elem;
+
+	if (!ht || !ht->array)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printed = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		elem = ht->array[i];
+		while (elem != NULL)
+		{
+			if (printed)
+				printf(", ");
+
+			printf("'%s': '%s'", elem->key, elem->value);
+			printed = 1;
+			elem = elem->next;
+		}
+	}
+
+	printf("}\n");
+}
